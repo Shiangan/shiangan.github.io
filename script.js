ff4821faaccb78e002e2160e43ffbf3a989246fe
Shiangan <avarubby85110@gmail.com>
2024-07-13 17:40:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-<script src="your-script.js"></script>
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "LocalBusiness",
-  "name": "祥安生命禮儀公司",
-  "description": "提供溫馨尊嚴的葬禮服務，包括佛道教、日蓮正宗、創價學會、基督教、天主教等各類宗教儀式。價格實惠，24小時免費諮詢服務。",
-  "url": "https://yourwebsite.com",
-  "logo": "https://yourwebsite.com/logo.png",
-  "contactPoint": {
-    "@type": "ContactPoint",
-    "telephone": "+886-938-179-858",
-    "contactType": "Customer Service"
-  },
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "Your Address",
-    "addressLocality": "New Taipei City",
-    "postalCode": "12345",
-    "addressCountry": "Taiwan"
-  }
-}
-</script>
+<!-- JavaScript 文件 -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/jarallax.min.js"></script>
+  <script src="assets/js/smooth-scroll.js"></script>
+  <script src="assets/js/theme.js"></script>
+</body>
+</html>
