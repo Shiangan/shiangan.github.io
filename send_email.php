3f955eeb5c910ce02f0344fc84d07c68ec10828c
Shiangan <avarubby85110@gmail.com>
2024-10-16 13:32:56 +0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,20 +1,25 @@
 <?php
-if($_POST) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $guestTitle = $_POST['guestTitle'];
     $name = $_POST['name'];
     $phone = $_POST['phone'];
-    $message = $_POST['textarea'];
-    $organization = implode(", ", $_POST['organization']);
-    $gender = $_POST['gender'];
-    
-    $to = "shiang_an24@yahoo.com.tw";
-    $subject = "新的表單提交";
-    $body = "姓名: $name\n聯絡電話: $phone\n諮詢內容: $message\n選擇儀式: $organization\n稱謂: $gender";
-    $headers = "From: 24hour@yahoo.com.tw";
+    $sokaGakkai = isset($_POST['sokaGakkai']) ? '是' : '否';
+    $nichiren = isset($_POST['nichiren']) ? '是' : '否';
+    $inquiry = $_POST['inquiry'];
 
-    if(mail($to, $subject, $body, $headers)) {
-        echo "感謝您的來信，禮儀師盡快回覆您！";
-    } else {
-        echo "Oops...! 有些問題，請稍後再試。";
-    }
+    $to = 'shiang_an24@yahoo.com.tw'; // 你的邮箱地址
+    $subject = '新表单提交';
+    $message = "
+    貴客稱呼: $guestTitle\n
+    姓名: $name\n
+    聯絡電話: $phone\n
+    創價學會: $sokaGakkai\n
+    日蓮正宗: $nichiren\n
+    想詢問的: $inquiry
+    ";
+    $headers = "From: no-reply@yourdomain.com\r\n";
+    mail($to, $subject, $message, $headers);
+    header("Location: thank_you.html"); // 提交后跳转的页面
+    exit();
 }
 ?>
