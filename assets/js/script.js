326e9fb9e75972f46227a55da9cbea6e62cf6f5c
Shiangan <avarubby85110@gmail.com>
2024-10-18 15:56:49 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-<!-- JavaScript 文件 -->
-  <script src="assets/js/jquery.min.js" defer></script>
+  <!-- JavaScript 文件 -->
+  <script src="assets/js/jquery.min.js"></script> <!-- 移除 defer，确保 jQuery 先加载 -->
   <script src="assets/js/popper.min.js" defer></script>
   <script src="assets/js/bootstrap.min.js" defer></script>
   <script src="assets/js/jarallax.min.js" defer></script>
