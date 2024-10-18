2a173b9cbb288e7eca9df3ee61a65199d5d6658d
Shiangan <avarubby85110@gmail.com>
2024-10-18 13:36:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 <!-- JavaScript 文件 -->
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/popper.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-  <script src="assets/js/jarallax.min.js"></script>
-  <script src="assets/js/smooth-scroll.js"></script>
-  <script src="assets/js/theme.js"></script>
+  <script src="assets/js/jquery.min.js" defer></script>
+  <script src="assets/js/popper.min.js" defer></script>
+  <script src="assets/js/bootstrap.min.js" defer></script>
+  <script src="assets/js/jarallax.min.js" defer></script>
+  <script src="assets/js/smooth-scroll.js" defer></script>
+  <script src="assets/js/theme.js" defer></script>
 </body>
 </html>
