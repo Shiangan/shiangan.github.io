dff059cd5ede05b340de0263d15e91aa851dbcda
Shiangan <avarubby85110@gmail.com>
2024-10-19 16:46:08 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-  <!-- JavaScript 文件 -->
-  <script src="assets/js/jquery.min.js"></script> <!-- 移除 defer，确保 jQuery 先加载 -->
-  <script src="assets/js/popper.min.js" defer></script>
-  <script src="assets/js/bootstrap.min.js" defer></script>
-  <script src="assets/js/jarallax.min.js" defer></script>
-  <script src="assets/js/smooth-scroll.js" defer></script>
-  <script src="assets/js/theme.js" defer></script>
-</body>
-</html>
+<!-- JavaScript 文件 -->
+<script src="assets/js/jquery.min.js"></script> <!-- 保证 jQuery 先加载 -->
+<script src="assets/js/popper.min.js" defer></script>
+<script src="assets/js/bootstrap.min.js" defer></script>
+<script src="assets/js/jarallax.min.js" defer></script>
+<script src="assets/js/smooth-scroll.js" defer></script>
+<script src="assets/js/theme.js" defer></script>
