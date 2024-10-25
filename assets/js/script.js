0109d6999696b9586cb25c3bf3f0619fc9f58937
Shiangan <avarubby85110@gmail.com>
2024-10-25 04:24:57 +0000

    Pending changes exported from your codespace

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-<!-- JavaScript 文件 -->
-<script src="assets/js/jquery.min.js"></script> <!-- 保证 jQuery 先加载 -->
-<script src="assets/js/popper.min.js" defer></script>
-<script src="assets/js/bootstrap.min.js" defer></script>
-<script src="assets/js/jarallax.min.js" defer></script>
-<script src="assets/js/smooth-scroll.js" defer></script>
-<script src="assets/js/theme.js" defer></script>
+
+<><script src="assets/js/jquery.min.js"></script>< /></>!-- 保证 jQuery 先加载 -->
+    <><script src="assets/js/popper.min.js" defer></script><script src="assets/js/bootstrap.min.js" defer></script><script src="assets/js/jarallax.min.js" defer></script><script src="assets/js/smooth-scroll.js" defer></script><script src="assets/js/theme.js" defer></script></>
